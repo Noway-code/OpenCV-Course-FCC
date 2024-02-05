9ec749184023a3dbf54e0e74723a56e3780353a3
noway <dragonfury1928@gmail.com>
2024-02-05 18:36:52 -0500

    Changed the size of rectangle

--- draw.py
+++ draw.py
@@ -9,12 +9,12 @@ cv.imshow('Blank', blank)  # Display the blank image
 # cv.imshow('Cyan', blank)  # Value in '' is the name of the window and blank is the image to be displayed.
 
 # Draw a rectangle using the blank image
-rectangle = np.zeros((500, 500, 3), dtype='uint8')
-rectangle[200:300, 100:500] = 0, 0, 255  # Change the color of the image to red. [200:300, 300:400] is the region of the image to be changed.
-cv.imshow('Gradient', rectangle)
+# rectangle = np.zeros((500, 500, 3), dtype='uint8')
+# rectangle[200:300, 100:500] = 0, 0, 255  # Change the color of the image to red. [200:300, 300:400] is the region of the image to be changed.
+# cv.imshow('Gradient', rectangle)
 
 # Draw a rectangle using a built-in function
-cv.rectangle(blank, (0, 0), (250, 250), (0, 255, 0), thickness=1)  # Draw a green rectangle on the blank image
+cv.rectangle(blank, (0, 0), (blank.shape[1]//2, blank.shape[0]//2), (0, 255, 0), thickness=-1)  # dimension of half of the image
 cv.imshow('Rectangle', blank)
 
 
